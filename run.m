f72450ac28cee23f6a0c809b0fb9d2f8052dfc44
Matt <mgolub@cmu.edu>
2018-03-13 20:04:26 -0700

    cleaned up exampleData; detailed data in comments

--- run.m
+++ run.m
@@ -64,6 +64,74 @@ cvx_setup % This line only needs to be run once and can be commented out thereaf
 %% Load data from the representative experiment.
 load exampleData.mat
 
+% The workspace now contains the following 3 variables: beforeLearning, 
+% afterLearning, and expParams.
+%
+% beforeLearning:
+%   A struct containing data from the 'before learning' trials of the 
+%   example experiment, which we defined as the last 50 trials under the 
+%   intuitive BCI mapping.
+%       .name: 'Data' because that's what we're talking about.
+%       .clouds: Data grouped into 8 movement-specific clouds. Most fields
+%          here are 8-element cell arrays, with cell{i} containing data
+%          from timesteps when the cursor-to-target direction was closer to
+%          .angles(i) than to any other element of .angles. A single trial
+%          can contribute data to multiple movement-specific clouds (if
+%          the cursor movement from that trial is not straight). In these
+%          8-element cell arrays, each cell contains an N-column matrix (in
+%          this experiment, N=71). In these matrices, each column (indexed
+%          by j in descriptions below) represents a single non-overlapping 
+%          45ms timestep.
+%
+%           .factorActivity{i}(:,j): 10D 'population activity pattern', 
+%              defined mathematically as \hat{z}_t^{orth} in equation 15.
+%           .cursorToTargetDirection{i}(:,j): 2D unit vector in the
+%              straight-to-target direction from the current cursor 
+%              position.
+%           .spikes{i}(:,j): q-dimensional vector of spike counts (here 
+%              q=86), which maps to .factorActivity{i}(:,j) after z-scoring
+%              and dimensionality reduction via factor analysis.
+%           .vraw_intuitive{i}(:,j): 2D single-timestep cursor velocity
+%              computed by passing .factorActivity{i}(:,j) through the
+%              intuitive BCI mapping. This corresponds to equation
+%              4, but replacing B^{pert} with B (i.e., the intuitive
+%              mapping, not the perturbed mapping).
+%           .vraw_perturbed{i}(:,j): 2D single-timestep cursor velocity
+%              computed by passing .factorActivity{i}(:,j) through the
+%              perturbed BCI mapping. This corresponds to equation 4.
+%           .intuitiveProgress{i}(:,j): The projection of
+%              .vraw_intuitive{i}(:,j) onto .cursorToTargetDirection{i}(:,j).
+%           .perturbedProgress{i}(:,j): The projection of
+%              .vraw_perturbed{i}(:,j) onto .cursorToTargetDirection{i}(:,j).
+%           .cursorProgress: Because the intuitive mapping was in place
+%              during the before-learning trials, this is exactly
+%              .intuitiveProgress.
+%       .angles: The 8 center-to-target angles (in degrees).
+%       .windowIdx: Trial numbers of the trials from which these data
+%           originated.
+%
+% afterLearning:
+%   A struct with the same format as beforeLearning, but with data from the
+%   'after learning' trials, which we defined as the successful trials from 
+%   the 50 consecutive perturbation trials that showed the best behavioral 
+%   performance (see Methods). Here, .cursorProgress is exactly
+%   .perturbedProgress (because the perturbed mapping was in place during
+%   the after-learning trials).
+%
+% expParams:
+%   A struct containing the following details from the example experiment:
+%       .monkName: The animals's name.
+%       .expDate: The experiment date.
+%       .intuitiveMapping: Struct containing components of the intuitive
+%           BCI mapping. Note: naming conventions here do not always match
+%           those from the paper.
+%       .perturbedMapping: Struct containing components of the perturbed
+%           BCI mapping. Naming conventions match those in .intuitiveMapping.
+%       .lambda: Scalar multiplier used to match predicted and actual 
+%           mean acquisition times from the after-learning trials. This is
+%           \lambda_{after}^{pert} in equation 9.
+
+
 %% Perform analyses on the data from the representative experiment.
 afterLearning.metrics = computePopulationMetrics(beforeLearning,afterLearning,expParams);
 
